3da0dadc4a9b13c3f451e6a823f225184553a4d3
prateek041 <prateeksingh9741@gmail.com>
2022-05-26 15:27:36 +0530

    Data types in GO. Time Stamp: 45:20
    - shorthand for variables
    - types, unsinged and signed
    - %T placeholder

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ import "fmt"
 func main() { // There can only be one main function per application.
 	// fmt.Println("Hello world") // "Print" is a function in the fmt package.
 
-	var conferenceName = "Go Conference" // Variable.
-	const conferenceTickets = 50         // Constant.
-	var remainingTickets = 50
+	// var conferenceName = "Go Conference"                               // Variable.
+	conferenceName := "Go conference"                                     // shorhand for variale declaration/assignment. has its restrictions.
+	const conferenceTickets uint = 50                                     // Constant.
+	var remainingTickets uint = 50                                        // Go can assign the type, depending upon the data assigned.
 	fmt.Printf("welcome to the %v booking application\n", conferenceName) // %v is the placeholder
 	fmt.Println("Get your tickets here")
 	fmt.Printf("%v tickets remaining from %v tickets\n", remainingTickets, conferenceTickets)
 
+	var userName string
+	var userTicket int
+	// ask for the user input
+
+	userName = "prateek"
+	userTicket = 5
+	fmt.Printf("The user %v booked %v tickets\n", userName, userTicket) // we can use %T to print the type
+	// of the given variable.
+	fmt.Printf("conferenceTicket is of type %T, conferenceName is of type %T\n", conferenceTickets, conferenceName)
 }
