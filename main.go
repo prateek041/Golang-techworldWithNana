56db80ac4f3898650761b89e432d2de597ac4b46
prateek041 <prateeksingh9741@gmail.com>
2022-05-26 14:57:02 +0530

    variables and constants in go
    declared and defined var, const and how to use comma to print output.
    Time stamp : 30:41

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import "fmt"
 // Go needs an entry point to start executing the program. It looks for a main function
 // to begin with.
 func main() { // There can only be one main function per application.
-	fmt.Println("Hello world") // "Print" is a function in the fmt package.
+	// fmt.Println("Hello world") // "Print" is a function in the fmt package.
+
+	var conferenceName = "Go Conference" // Variable.
+	const conferenceTickets = 50         // Constant.
+	var remainingTickets = 50
+	fmt.Println("welcome to the", conferenceName, "booking application")
+	fmt.Println("Get your tickets here to print")
+	fmt.Println(conferenceTickets, "tickets remaining")
 
 }
